1aa8f3207110af4d6103d07c6fb08d3d6cc8120a
Sam Ho <samho@peoplematch.me>
2012-02-21 17:27:53 -0800

    Created default user Sam and meetup_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'Sam Ho', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
+
+puts 'SETTING UP DEFAULT MEETUP USER'
+MeetupMember.create!(:name => "Sam Ho", 
+                  :user_id => 1,
+                  :meetup_id => 6442685, 
+                  :unparsed_json => [381, 4422, 9696, 10102, 9760, 55324, 18062, 37381, 29971, 115913, 30928, 456, 98380, 10538, 77098, 38968, 21681, 91146, 18060, 15167, 27591, 121802, 20837, 17158, 46616, 38660, 57242, 29000, 99211, 99649, 155621, 62649],
+                  :image_url => "http://photos1.meetupstatic.com/photos/member/5/9/e/member_5707438.jpeg", 
+                  :linkedin_url => "http://www.linkedin.com/in/samho",
+                  :twitter => "@sammho"   )
+
 
