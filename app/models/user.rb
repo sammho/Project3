d3db70a4da06bae85b272fdf75ce3b5770fe9af5
Sam Ho <samho@peoplematch.me>
2012-03-02 16:26:17 -0800

    Initial calculation of affinity score is complete, now should display ranked by affinity score

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,58 +37,63 @@ class User < ActiveRecord::Base
   def calculate_affinity(target_member)
 
     if affinity = UserAffinity.find_by_user_id_and_t_meetup_member_id(self.id, target_member.meetup_id)
+
+      ## TODO: for now, let's always recalulate affinity scores (particularly to populate empties)
       puts "Affinity exists for #{target_member.name} is #{affinity.affinity_score}\n"
+
       
       ## TODO: Add additional condition to recalculate if update is > 1 month
-      return affinity
     else
-      @common_topics = self.topics_in_common(target_member)
-
       affinity = UserAffinity.new
-      affinity.user_id = id
-      affinity.t_meetup_member_id = target_member.meetup_id
-      affinity.meetup_topics_in_common = @common_topics
-      affinity.linked_in = !target_member.linkedin_url.nil? 
-      affinity.twitter = !target_member.twitter.nil? 
-
-      @common_topics.count
-      total_user_topics = MeetupMember.find_by_user_id(id).unparsed_json.count 
-      target_member.unparsed_json.count 
-
-      ## Affinity score calculation
-      ## .1 * Twitter + .2 * LinkedIn + .5 * % in common + .2 * (# in common - max 20)
-
-      if @common_topics.count >= 20
-        normalized_common_topics_count = 20
-      else
-        normalized_common_topics_count = @common_topics.count
-      end
-
-      affinity.save
-
-      twitter_score = (affinity.twitter ? 10.0 : 0.0) 
-      linkedin_score = (affinity.linked_in ? 20.0 : 0.0) 
-      topics_score = 50.0 * (@common_topics.count.to_f / (self.topics.count + 1.0))
-      abs_topics_score = 20.0 * (normalized_common_topics_count.to_f / 20.0)
-
-      #affinity.affinity_score = (affinity.twitter ? 10 : 0) +
-      score = (affinity.twitter ? 10 : 0) +
-                                (affinity.linked_in ? 20 : 0) +
-                                50 * (@common_topics.count / (self.topics.count + 1)) +
-                                20 * (normalized_common_topics_count / 20)
-
-      puts "\nCalculating score for #{target_member.name}\n"
-      puts "Scores are Twitter #{twitter_score} LI: #{linkedin_score} topics #{topics_score} abs #{abs_topics_score}\n"
-      puts "Twitter is #{(affinity.twitter ? 10 : 0)}\n" 
-      puts "Linked in is #{(affinity.linked_in ? 20 : 0)}\n"
-      puts "Common topics count is #{@common_topics.count}\n"
-      puts "Self topics count is #{self.topics.count}\n"
-      puts "% common is #{50 * (@common_topics.count / (self.topics.count + 1))}\n"
-      puts "Abs % is #{20 * (normalized_common_topics_count / 20)}\n"
-
-      puts "Affinity of #{target_member.name} is #{score}\n"
-
     end
+
+    ## TODO: Do I really need to update all these same fields?
+    @common_topics = self.topics_in_common(target_member)
+    
+    affinity.user_id = id
+    affinity.t_meetup_member_id = target_member.meetup_id
+    affinity.meetup_topics_in_common = @common_topics
+    affinity.linked_in = !target_member.linkedin_url.nil? 
+    affinity.twitter = !target_member.twitter.nil? 
+
+    @common_topics.count
+    total_user_topics = MeetupMember.find_by_user_id(id).unparsed_json.count 
+    target_member.unparsed_json.count 
+
+    ## Affinity score calculation
+    ## .1 * Twitter + .2 * LinkedIn + .5 * % in common + .2 * (# in common - max 20)
+
+    if @common_topics.count >= 20
+      normalized_common_topics_count = 20
+    else
+      normalized_common_topics_count = @common_topics.count
+    end
+
+
+    twitter_score = (affinity.twitter ? 10.0 : 0.0) 
+    linkedin_score = (affinity.linked_in ? 20.0 : 0.0) 
+    topics_score = 50.0 * (@common_topics.count.to_f / (self.topics.count + 1.0))
+    abs_topics_score = 20.0 * (normalized_common_topics_count.to_f / 20.0)
+
+    #affinity.affinity_score = (affinity.twitter ? 10 : 0) +
+    affinity.affinity_score = (affinity.twitter ? 10.0 : 0.0) +
+                              (affinity.linked_in ? 20.0 : 0.0) +
+                              50.0 * (@common_topics.count / (self.topics.count + 1.0)) +
+                              20.0 * (normalized_common_topics_count / 20.0)
+
+    puts "\nCalculating score for #{target_member.name}\n"
+    puts "Scores are Twitter #{twitter_score} LI: #{linkedin_score} topics #{topics_score} abs #{abs_topics_score}\n"
+    puts "Twitter is #{(affinity.twitter ? 10 : 0)}\n" 
+    puts "Linked in is #{(affinity.linked_in ? 20 : 0)}\n"
+    puts "Common topics count is #{@common_topics.count}\n"
+    puts "Self topics count is #{self.topics.count}\n"
+    puts "% common is #{50.0 * (@common_topics.count / (self.topics.count + 1.0))}\n"
+    puts "Abs % is #{20.0 * (normalized_common_topics_count / 20.0)}\n"
+
+    puts "Affinity of #{target_member.name} is #{affinity.affinity_score}\n"
+
+    affinity.save
+
   end
 end
 
