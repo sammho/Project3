3027fe34626b8217c194a558eb77a476bf93d240
Sam Ho <samho@peoplematch.me>
2012-03-13 13:21:25 -0700

    Completed helper method to print topics and url links.  Currently url links do not work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,26 @@ module ApplicationHelper
     topic_objects = [] 
     total = topics_id_array.count
 
-    while i < num_topics || i >= total
+    while i < num_topics 
       topic = MeetupTopic.find_by_meetup_id(topics_id_array[i])
       topic_objects << topic
       i += 1
+      break if i >= total
     end
 
     return topic_objects
+  end
+
+  ## Given an array of meetup topics, output the topic names linked to their URLs separated by a space
+  def print_meetup_topics(topics)
+    topic_string = ""
+
+    topics.each do |topic|
+      topic_string << "#{link_to(topic.name, topic.urlkey)} "
+    end
+    
+    return raw(topic_string)
+
   end
 
 end
