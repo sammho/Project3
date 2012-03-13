e9e78fa405d2236ac62ac1517b90f898761bb55e
Sam Ho <samho@peoplematch.me>
2012-03-13 12:46:25 -0700

    Created helper method to get random subset of meetup_topics given an array of topic IDs.  This will be used to only display a predetermined subset of topics to a user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
 
+  ## Given an array of meetup of topics, return a randomized subset up to the number specified
+  def get_rand_meetup_topics(topics_id_array, num_topics)
+    ## The easy way is to return the first number up to index
+    i = 0
+    topic_objects = [] 
+    total = topics_id_array.count
+
+    while i < num_topics || i >= total
+      topic = MeetupTopic.find_by_meetup_id(topics_id_array[i])
+      topic_objects << topic
+      i += 1
+    end
+
+    return topic_objects
+  end
+
 end
